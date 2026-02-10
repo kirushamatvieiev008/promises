b1beb5eee0f34f9b975cdea967c2c99e09259497
kirushamatvieiev008 <mkiril087@gmail.com>
2026-02-10 11:44:33 +0200

    Updates

--- promises.0f77c784.js
+++ promises.0f77c784.js
@@ -816,17 +816,65 @@ const toggleUserState = (allUsers, userName)=>{
     });
 };
 const logger = (updatedUsers)=>console.table(updatedUsers);
-/*
- * Зараз працює так
- */ toggleUserState(users, 'Mango').then(logger);
+// /*
+//  * Зараз працює так
+//  */
 toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Mango').then(logger); // toggleUserState(users, 'Mango', logger);
- // toggleUserState(users, 'Mango', logger);
- // toggleUserState(users, 'Lux', logger);
- /*
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+/*
  * Повинно працювати так
- */  // toggleUserState(users, 'Mango').then(logger);
- // toggleUserState(users, 'Lux').then(logger);
+ */ // toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
+const randomIntegerFromInterval = (min, max)=>{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const makeTransaction = (transaction)=>{
+    const delay = randomIntegerFromInterval(200, 500);
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{
+            const canProcess = Math.random() > 0.3;
+            console.log(transaction);
+            if (canProcess) res(transaction.id, delay);
+            else rej(transaction.id);
+        // res(transaction.id, delay);
+        // rej(transaction.id);
+        }, delay);
+    });
+};
+const logSuccess = (id, time)=>{
+    console.log(`Transaction ${id} processed in ${time}ms`);
+};
+const logError = (id)=>{
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+/*
+ * Працює так
+ */ // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Повинно працювати так
+ */ makeTransaction({
+    id: 70,
+    amount: 150
+}).then(logSuccess).catch(logError);
+makeTransaction({
+    id: 71,
+    amount: 230
+}).then(logSuccess).catch(logError);
+makeTransaction({
+    id: 72,
+    amount: 75
+}).then(logSuccess).catch(logError);
+makeTransaction({
+    id: 73,
+    amount: 100
+}).then(logSuccess).catch(logError);
 
 },{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire8b27", {})
 
